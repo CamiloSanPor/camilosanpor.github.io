2e31c13e8bdee09219d62aa39a9f98eafc6f193e
CamiloSanPor <camilosanpor@gmail.com>
2024-03-14 22:00:44 -0300

    Random Pokemon Subpage
    Wanted to test using PokeAPI.
    Created a subpage because I don't want to create a new github account just to host it.

---
+++ RandomPokeAPI/randomPokemon.js
@@ -0,0 +1,69 @@
+function getRandomPokemon(){
+
+    const random = Math.floor(Math.random() * (1025 - 1) + 1);
+
+    let apiUrl = `https://pokeapi.co/api/v2/pokemon/${random}/`;
+
+    return apiUrl;
+}
+
+function displayPokemon(data){
+    const body = document.body;
+    const pokemon = document.createElement('div');
+    const name = document.createElement('p');
+    const sprite = document.createElement('img');
+    const audio = document.createElement('audio');
+    const refresh = document.createElement('button');
+
+    name.innerHTML = data.name.toUpperCase();
+    refresh.innerHTML = '🔄';
+
+    sprite.setAttribute('src', data.sprites.front_default);
+    sprite.setAttribute('height', '200px');
+
+    body.appendChild(refresh);
+    body.appendChild(pokemon);
+    
+    pokemon.style.textAlign = 'center';
+    pokemon.appendChild(name);
+    pokemon.appendChild(sprite);
+    
+    refresh.addEventListener('click', (event) => {
+        location.reload();
+    });
+
+    audio.setAttribute('src', data.cries.latest);
+    audio.addEventListener('canplaythrough', (event) => {
+        audio.play();
+    });
+
+    sprite.appendChild(audio);
+    sprite.addEventListener('click', (event) => {
+        audio.play();
+        console.log('clicked');
+    });
+
+}
+
+// API endpoint URL
+const apiUrl = getRandomPokemon();
+
+// Make a GET request to the API endpoint
+fetch(apiUrl)
+  .then(response => {
+    // Check if the response is successful (status code between 200 and 299)
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    // Parse the JSON response
+    return response.json();
+  })
+  .then(data => {
+    // Handle the JSON data
+    console.log(data);
+    displayPokemon(data);
+  })
+  .catch(error => {
+    // Handle errors
+    console.error('There was a problem with the fetch operation:', error);
+  });
