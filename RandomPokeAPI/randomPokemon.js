712915b69bf588735d5b1ebc318a5b354e7f4f80
Camilo <camilosanpor@gmail.com>
2025-03-01 13:28:00 -0400

    Fun Pokemon Page

--- RandomPokeAPI/randomPokemon.js
+++ RandomPokeAPI/randomPokemon.js
@@ -42,30 +42,78 @@ function displayPokemon(data){
     sprite.appendChild(audio);
     sprite.addEventListener('click', (event) => {
         audio.play();
-        console.log('clicked');
     });
 
 }
 
-// API endpoint URL
-const apiUrl = getRandomPokemon();
+// set background color based on the pokemons type
+function setBackgroundColor(data){
 
-// Make a GET request to the API endpoint
-fetch(apiUrl)
-  .then(response => {
-    // Check if the response is successful (status code between 200 and 299)
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    // Parse the JSON response
-    return response.json();
-  })
-  .then(data => {
-    // Handle the JSON data
-    console.log(data);
-    displayPokemon(data);
-  })
-  .catch(error => {
-    // Handle errors
-    console.error('There was a problem with the fetch operation:', error);
-  });
+  let pkmnType = data.types[0].type.name;
+
+  let typeColors = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD'
+};
+
+
+  console.log(pkmnType);
+  console.log(typeColors[pkmnType]);
+
+  document.body.style.backgroundColor = typeColors[pkmnType];
+
+}
+
+function fetchPokeAPI(){
+
+  // API endpoint URL
+  const apiUrl = getRandomPokemon();
+
+  // Make a GET request to the API endpoint
+  fetch(apiUrl)
+    .then(response => {
+      // Check if the response is successful (status code between 200 and 299)
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Parse the JSON response
+      return response.json();
+    })
+    .then(data => {
+      // Handle the JSON data
+      console.log(data);
+      displayPokemon(data);
+      setBackgroundColor(data);
+    })
+    .catch(error => {
+      // Handle errors
+      console.error('There was a problem with the fetch operation:', error);
+    });
+
+}
+
+document.addEventListener('keydown', function(event) {
+  // Check if the pressed key is spacebar (keycode 32)
+  if (event.key === ' ') {
+      // Run your JavaScript function here
+      this.location.reload();
+  }
+});
+
+fetchPokeAPI();
